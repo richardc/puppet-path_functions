c376dace6ba52926e2023a513ec0c4dcfeefe75d
Richard Clamp <richardc@unixbeard.net>
2013-03-14 20:37:17 +0000

    make source_path work
    Needs error handling for non-existent modules and such

--- lib/puppet/parser/functions/source_path.rb
+++ lib/puppet/parser/functions/source_path.rb
@@ -1,5 +1,8 @@
 Puppet::Parser::Functions::newfunction(:source_path, :type => :rvalue) do |args|
   args.collect do |uri|
-    uri
+    uri =~ /^puppet:\/\/.*?\/modules\/(.*?)\/(.*)/ or next
+    module_name = $1
+    relative_path = $2
+    self.environment.module(module_name).path + "/files/" + relative_path
   end
 end
